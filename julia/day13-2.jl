aaf4f1c37e1d2b30932fe86e0cf21c39cde44c64
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2018-12-14 00:24:13 -0300

    Day 13-2: Last Cart Standing

---
+++ julia/day13-2.jl
@@ -0,0 +1,174 @@
+function last_cart_standing(input)
+    board = parse_input(input)
+
+    while true
+        tick(board)
+        if length(board.carts) == 1
+            return board.carts[1]
+        end
+    end
+    return ""
+end
+
+function tick(board)
+    # print_board(board)
+    sort!(board.carts, by=c -> (c.y, c.x))
+    for cart in board.carts
+        move(cart, board)
+        board.carts = remove_collisions(board.carts)
+    end
+end
+
+function remove_collisions(carts)
+    positions = Dict{Tuple{Int, Int}, Array{Cart}}()
+    for cart in carts
+        pos = (cart.x, cart.y)
+        if !haskey(positions, pos)
+            positions[pos] = []
+        end
+        push!(positions[pos], cart)
+    end
+    carts::Array{Cart} = []
+    for c in values(positions)
+        if length(c) == 1
+            push!(carts, c[1])
+        end
+    end
+    return carts
+end
+
+function move(cart, board)
+    update_position(cart)
+    track = board.tracks[cart.y+1][cart.x+1]
+    if track == '/' || track == '\\'
+        turn(cart, track)
+    elseif track == '+'
+        increment_next_turn(cart)
+    end
+end
+
+function turn(cart, track)
+    if (cart.v == '^' && track == '/') ||
+        (cart.v == '>' && track == '\\') ||
+        (cart.v == 'v' && track == '/') ||
+        (cart.v == '<' && track == '\\')
+            direction = 'R'
+    else
+            direction = 'L'
+    end
+    rotate(cart, direction)
+end
+
+function rotate(cart, direction)
+    if direction == 'L'
+        if cart.v == '<'
+            cart.v = 'v'
+        elseif cart.v == 'v'
+            cart.v = '>'
+        elseif cart.v == '>'
+            cart.v = '^'
+        elseif cart.v == '^'
+            cart.v = '<'
+        end
+    elseif direction == 'R'
+        if cart.v == '<'
+            cart.v = '^'
+        elseif cart.v == '^'
+            cart.v = '>'
+        elseif cart.v == '>'
+            cart.v = 'v'
+        elseif cart.v == 'v'
+            cart.v = '<'
+        end
+    end
+end
+
+function update_position(cart)
+    if cart.v == '>'
+        cart.x += 1
+    elseif cart.v == '<'
+        cart.x -= 1
+    elseif cart.v == 'v'
+        cart.y += 1
+    elseif cart.v == '^'
+        cart.y -= 1
+    end
+end
+
+function increment_next_turn(cart)
+    if cart.next_turn == 'L'
+        rotate(cart, cart.next_turn)
+        cart.next_turn = 'S'
+    elseif cart.next_turn == 'R'
+        rotate(cart, cart.next_turn)
+        cart.next_turn = 'L'
+    else
+        cart.next_turn = 'R'
+    end
+end
+
+function print_board(board)
+    for row in board.tracks
+        for ch in row
+            print(ch)
+        end
+        println()
+    end
+    println(board.carts)
+end
+
+function parse_input(input)
+    rows::Array{Array{Char}} = []
+    carts::Array{Cart} = []
+    
+    y = -1
+    for row in input
+        y += 1
+        x = -1
+        tracks::Array{Char} = []
+        for ch in row
+            x += 1
+            if ch == ' ' || ch == '|' || ch == '-' || ch == '/' || ch == '\\' || ch == '+'
+                push!(tracks, ch)
+            elseif ch == '>' || ch == '<'
+                push!(tracks, '-')
+                push!(carts, Cart(x, y, ch, 'L'))
+            elseif ch == 'v' || ch == '^'
+                push!(tracks, '|')
+                push!(carts, Cart(x, y, ch, 'L'))
+            end
+        end
+        push!(rows, tracks)
+    end
+
+    return Board(rows, carts)
+end
+
+mutable struct Cart
+    x::Int
+    y::Int
+    v::Char
+    next_turn::Char
+end
+
+mutable struct Board
+    tracks::Array{Array{Char}}
+    carts::Array{Cart}
+end
+
+function main()
+    filename = ARGS[1]  # julia arrays are 1-indexed!
+    input = split(read(filename, String), "\n")
+    test_input = [
+"/->-\\        ",
+"|   |  /----\\",
+"| /-+--+-\\  |",
+"| | |  | v  |",
+"\\-+-/  \\-+--/",
+"  \\------/  ",
+    ]
+
+    println(last_cart_standing(input))
+end
+
+main()
