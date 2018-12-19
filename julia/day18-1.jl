fb785bff0a73c298812f042974b2076105126bf0
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2018-12-19 00:03:59 -0500

    Day 18-1: Conway's Game of North Pole
    17 is a huge pain so i'll do it later don't judge me k

---
+++ julia/day18-1.jl
@@ -0,0 +1,123 @@
+function conways_north_pole(input)
+    board = parse_input(input)
+    turns = 0
+    while turns != 10
+        turns += 1
+        board = process_turn(board)
+    end
+
+    lumber = count_type(board, '#')
+    wood = count_type(board, '|')
+    println("$lumber $wood")
+    return lumber * wood
+end
+
+struct Board
+    tiles::Array{Array{Char}}
+end
+
+function Base.show(io::IO, b::Board)
+    for row in b.tiles
+        for c in row
+            print(c)
+        end
+        println()
+    end
+end
+
+function count_type(board, type)
+    total = 0
+    for row in board.tiles
+        total += count(c -> c==type, row)
+    end
+    return total
+end
+
+function process_turn(board)
+    new_rows = []
+    for y in 1:length(board.tiles)
+        new_row = []
+        for x in 1:length(board.tiles[y])
+            ch = get_next(board, x,y)
+            push!(new_row, ch)
+        end
+        push!(new_rows, new_row)
+    end
+    return Board(new_rows)
+end
+
+function get_next(board, x,y)
+    ch = board.tiles[y][x]
+    surroundings = []
+    minx = max(1, x-1)
+    maxx = min(length(board.tiles[1]), x+1)
+    miny = max(1, y-1)
+    maxy = min(length(board.tiles), y+1)
+
+    for iy in miny:maxy
+        for ix in minx:maxx
+            if iy == y && ix == x
+                continue
+            end
+            push!(surroundings, board.tiles[iy][ix])
+        end
+    end
+
+    # println(surroundings)
+    if ch == '.'
+        # open
+        if count(c -> c=='|', surroundings) >= 3
+            return '|'
+        else
+            return ch
+        end
+    elseif ch == '|'
+        if count(c -> c=='#', surroundings) >= 3
+            return '#'
+        else
+            return ch
+        end
+    else
+        if any(c -> c=='|', surroundings) && any(c -> c=='#', surroundings) 
+            return ch
+        else
+            return '.'
+        end
+    end
+end
+
+function parse_input(input)
+    rows = []
+    for line in input
+        if line == ""
+            break
+        end
+        row = []
+        for ch in line
+            push!(row, ch)
+        end
+        push!(rows, row)
+    end
+    return Board(rows)
+end
+
+function main()
+    filename = ARGS[1]  # julia arrays are 1-indexed!
+    input = split(read(filename, String), "\n")
+    test_input = [
+        ".#.#...|#.",
+        ".....#|##|",
+        ".|..|...#.",
+        "..|#.....#",
+        "#.#|||#|#|",
+        "...#.||...",
+        ".|....|...",
+        "||...#|.#|",
+        "|.||||..|.",
+        "...#.|..|.",
+    ]
+
+    println(conways_north_pole(input))
+end
+
+main()
